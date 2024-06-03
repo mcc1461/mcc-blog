43916c8e9a8082e67f966a302541691f6a435dc9
mcc1461 <mcoskuncelebi@gmail.com>
2024-06-03 22:32:53 +0300

    fix: logo file arranged

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const router = createBrowserRouter([
     path: "/",
     element: (
       <div style={{margin:"5rem"}}>
-        <h1>Hello World</h1>
+        <h1>mcc-blog</h1>
         <img src={ttt} style={{width:"95px", height:"95px", borderRadius:"50%"}}alt="ttt" />
         <Link to="about">About Us</Link>
       </div>
